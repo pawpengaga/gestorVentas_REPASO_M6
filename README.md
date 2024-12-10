3c69c4eb740755cdf0259c538f19310a47df5618
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-10 20:27:32 -0300

    repo venta listo

--- README.md
+++ README.md
@@ -13,4 +13,21 @@ Aplicacion de gestión de punto de venta con Usuarios y Detalle
 - SCHEMA crea la estructura de las tablas
 - DATA crea los primeros registros
 
+## Curiosidades
+
+Las cosas que nos ahorra `@Autowired`
+
+```java
+
+// Crea el objeto `JdbcTemplate` como constante...
+private final JdbcTemplate jdbc;
+
+// Asignarlo al constructor de manera manual...
+public SaleRepository(JdbcTemplate jdbc){
+  this.jdbc = jdbc;
+}
+
+```
+
+
 > Proyecto extraño pero muy interesante
---
+++ src/main/java/com/pawpengaga/repository/SaleRepository.java
@@ -0,0 +1,30 @@
+package com.pawpengaga.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.pawpengaga.model.Sale;
+
+@Repository
+public class SaleRepository {
+
+  @Autowired
+  JdbcTemplate jdbc;
+
+  // Este sera distinto por el uso de clave foranea...
+
+  public List<Sale> findAll(){
+    String sql = "SELECT * FROM sales";
+    return jdbc.query(sql, new BeanPropertyRowMapper<>(Sale.class)); // El equivalente al rowMapper al parecer...
+  }
+
+  public int save(Sale saleInput){
+    String sql = "INSERT INTO sales (user_id, fecha_venta) VALUES (?, ?)";
+    return jdbc.update(sql, saleInput.getUserId(), saleInput.getFechaVenta());
+  }
+
+}
