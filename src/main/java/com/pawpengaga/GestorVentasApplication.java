34eb879087d44a88d72caf229b014a8da2674329
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-10 19:17:33 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/GestorVentasApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpengaga/GestorVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
